a497d41c44c6348d1f3c445f85eabe917456a9b4
amirazain <amirazain2009@gmail.com>
2017-06-28 17:41:43 +0800

    adjustment on cards scss

--- mdb.js
+++ mdb.js
@@ -6258,7 +6258,7 @@ Waves.init(); /* FORMS */
 		Materialize.updateTextFields = function () {
 			var input_selector = 'input[type=text], input[type=password], input[type=email], input[type=url], input[type=tel], input[type=number], input[type=search], textarea';
 			$(input_selector).each(function (index, element) {
-				if ($(element).val().length > 0 || $(this).attr('placeholder') !== undefined || $(element)[0].validity.badInput === true) {
+				if ($(element).val().length > 0 || $(this).attr('placeholder') !== undefined || $(this).attr('v-model') !== undefined || $(element)[0].validity.badInput === true) {
 					$(this).siblings('label, i').addClass('active');
 				} else {
 					$(this).siblings('label, i').removeClass('active');
@@ -6291,7 +6291,7 @@ Waves.init(); /* FORMS */
 			if (formReset.is('form')) {
 				formReset.find(input_selector).removeClass('valid').removeClass('invalid');
 				formReset.find(input_selector).each(function () {
-					if ($(this).attr('value') === '') {
+					if ($(this).attr('value') === '' || $(this).attr('v-model') === '' ) {
 						$(this).siblings('label, i').removeClass('active');
 					}
 				});
